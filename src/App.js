c6da9fd64be4837b1f9ba44aa3487479df38c61f
M Noman Khalid <bhattinoman581@gmail.com>
2023-11-14 22:52:49 -0800

    work on redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import {Routes, Route} from 'react-router-dom'
+import Products from "./components/Products";
+import DetailPage from "./components/DetailPage";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/products" element={<Products/>}/>
+        <Route path="/products/:id" element={<DetailPage/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
+      </Routes>
+      <Footer/>
+      
     </>
   );
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Skeleton from "react-loading-skeleton";
+import { NavLink } from "react-router-dom";
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -64,21 +65,22 @@ const Products = () => {
           return (
             <>
               <div className="col-md-3 mb-4">
-                <div class="card h-100 text-center p-4 " key={product.id}>
+                <div className="card h-100 text-center p-4 " key={product.id}>
+                <NavLink to={`/products/${product.id}`}>
                   <img
                     src={product.image}
-                    class="card-img-top"
+                    className="card-img-top"
                     alt={product.title}
                     height="250px"
-                  />
-                  <div class="card-body">
-                    <h5 class="card-title mb-0">
+                  /></NavLink>
+                  <div className="card-body">
+                    <h5 className="card-title mb-0">
                       {product.title.substring(0, 12)}...
                     </h5>
-                    <p class="card-text lead fw-bold">${product.price}</p>
-                    <a href="#" class="btn btn-outline-dark">
+                    <p className="card-text lead fw-bold">${product.price}</p>
+                    <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">
                       Buy Now
-                    </a>
+                    </NavLink>
                   </div>
                 </div>
               </div>
@@ -91,7 +93,7 @@ const Products = () => {
 
   return (
     <div>
-      <div className="container my-5 py-5">
+      <div className="container products my-5 py-5">
         <div className="row">
           <div className="col-12 mb-5">
             <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
